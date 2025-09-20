a01c3dd20af79cd3e6751601aebcb289427ce0d1
dariusmocan <dariusmocan2003@gmail.com>
2025-09-20 23:14:49 +0300

    added prediction of user typed house

--- src/boston.py
+++ src/boston.py
@@ -23,7 +23,7 @@ print("r2_score : ", r2_score(y_test,y_pred))
 
 # print(df[["TAX"]].describe())
 
-
+# hardcoded house pred
 new_house = {
     "CRIM": 0.1,        # Crime rate
     "ZN": 18.0,         # Proportion of residential land zoned for large lots
@@ -52,3 +52,31 @@ for feature, value in new_house.items():
     print(f"  {feature}: {value}")
 
 print(f"\nPrice Prediction: ${price_pred:.2f}k")
+
+
+# user input house pred
+print("\n" + "="*50)
+print("INSERT NEW HOUSE DATA")
+print("="*50)
+
+user_house = {}
+for feature in X.columns:
+    while True:
+        try:
+            value = float(input(f"INSERT {feature}: "))
+            user_house[feature] = value
+            break
+        except ValueError:
+            print("Insert a valid number")
+
+user_house_df = pd.DataFrame([user_house])
+user_house_df = user_house_df[X.columns]
+
+user_log_price_pred = lr.predict(user_house_df)[0]
+user_price_pred = np.exp(user_log_price_pred)
+
+# print("\n Custom house inserted by user")
+# for feature, value in user_house.items():
+#     print(f"  {feature}: {value}")
+
+print(f"\nPrice Prediction: ${user_price_pred:.2f}k")
